47983a12a223e18f602094b06701c2b232e7d0e1
harman <harmansingh1099@gmail.com>
2025-11-13 09:34:34 -0500

    Created RegularTopping class

---
+++ src/main/java/com/pluralsight/RegularTopping.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+public class RegularTopping implements Topping{
+    private String name;
+
+    public  RegularTopping(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public double getPrice(String sandwichSize) {
+        // Regular toppings/sauces/sides are included
+        return 0.0;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean isExtra() {
+        return false;
+    }
+}
