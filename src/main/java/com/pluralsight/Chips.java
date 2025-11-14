4e2faf8f62ebb3c339bccfed59fec5a69a34d7d2
harman <harmansingh1099@gmail.com>
2025-11-13 19:53:36 -0500

    Created Chips and Drink classes

--- src/main/java/com/pluralsight/Chips.java
+++ src/main/java/com/pluralsight/Chips.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
-public class Chips {
+public class Chips implements OrderItem{
+    private String type;
+
+    public Chips(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public double getPrice() {
+        return 1.50;
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format("CHIPS: %s (%.2f)", type, getPrice());
+    }
 }
--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
-public class Drink {
+public class Drink implements OrderItem {
+    private String size;
+    private String flavor;
+
+    public Drink(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    @Override
+    public double getPrice() {
+        return switch (size) {
+            case "Small" -> 2.00;
+            case "Medium" -> 2.50;
+            case "Large" -> 3.00;
+            default -> 0.0;
+        };
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format("Drink: %s %s (%.2f)", size, flavor, getPrice());
+    }
 }
